66aba8884ca5d3106c836cc5eb16984dfabd52b9
Augustine Osagie <osagie98@gmail.com>
2019-07-11 23:14:18 -0500

    Tested database call

--- fwitter/api/create.py
+++ fwitter/api/create.py
@@ -4,10 +4,22 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 
-from . import api_bp
+import flask
 
-@api_bp.route('/create', methods=['GET'])
+from . import api_bp
+import fwitter
+
+@api_bp.route('/create', methods=['GET', 'POST'])
 def create():
-    print('test')
-    return 'Hello Blueprint!'
+
+    if flask.request.method == 'GET':
+        cur = fwitter.db.get_db().cursor()
+        cur.execute('select * from users')
+        test = cur.fetchall()
+        print(test[0]['username'])
+        
+        return 'Hello Blueprint!'
+    else:
+
+        return 'This is a POST'
     
