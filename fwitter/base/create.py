b7310f12d1529f7a197a47046f09a28694ecb971
Augustine Osagie <osagie98@gmail.com>
2019-07-13 12:03:24 -0400

    Added a get method for account create

--- fwitter/base/create.py
+++ fwitter/base/create.py
@@ -10,30 +10,33 @@ import fwitter
 import requests
 from fwitter.util.encrypt import hash_password
 
-@base_bp.route('/account/create', methods=['POST'])
+@base_bp.route('/account/create', methods=['GET', 'POST'])
 def create():
     """Add a new user to the database"""
 
-    request_data = flask.request.form
-        
-    fullname = request_data['fullname']
-    username = request_data['username']
-    email = request_data['email']
-    password = hash_password(request_data['password'])
-    filename = request_data['filename']
+    if flask.request.method == 'GET':
+        return flask.render_template("create.html")
+    else:
+        request_data = flask.request.form
+            
+        fullname = request_data['fullname']
+        username = request_data['username']
+        email = request_data['email']
+        password = hash_password(request_data['password'])
+        filename = request_data['filename']
 
-    db = fwitter.db.get_db()
-    cur = db.cursor()
-        
-    #TODO Fix returns
+        db = fwitter.db.get_db()
+        cur = db.cursor()
+            
+        #TODO Fix returns
 
-    # Because username is the primary key in users, the code throws an exception if a duplicate is added
-    try:
-        cur.execute("INSERT INTO users(fullname, username, email, password, filename) VALUES ('{}', '{}', '{}', '{}', '{}')".format(fullname, username, email, password, filename))
-    except:
-        return {}, 403
+        # Because username is the primary key in users, the code throws an exception if a duplicate is added
+        try:
+            cur.execute("INSERT INTO users(fullname, username, email, password, filename) VALUES ('{}', '{}', '{}', '{}', '{}')".format(fullname, username, email, password, filename))
+        except:
+            return {}, 403
 
-    return {}, 201
+        return {}, 201
 
 #Only temporary
 @base_bp.route('/')
