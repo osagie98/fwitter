5e776421e13f9b059c36f76cf23096f7e722b737
Augustine Osagie <osagie98@gmail.com>
2019-07-14 19:28:33 -0400

    Added login test

--- tests/test_session.py
+++ tests/test_session.py
@@ -7,11 +7,20 @@ from flask import session
 class TestSession():
     """Testing logging in and out"""
 
-    def test_login(self, app, client):
+    def test_login(self, app, client, cookie):
         """Testing that a user has access to the correct locations when logging"""
 
         test_username = 'osagie_01'
         test_password = 'thisIsATestPassword'
 
-        # Assert some areas can be accessed without logging in
+        # Assert a logged in user cannot log in again
+        response = cookie.login(test_username, test_password)
+       
+        with client:
+           client.get('/')
+           assert session['username'] == 'osagie_01'
+           assert session['email'] == 'osagie@umich.edu'
+           assert session['fullname'] == 'Augustine Osagie'
+
+        # TODO add error if logged in user logs in again
         
