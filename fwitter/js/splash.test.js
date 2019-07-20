52d0729d2a4b1a3f597eaf4c90a01ec9f0d148c0
Augustine Osagie <osagie98@gmail.com>
2019-07-19 23:55:25 -0500

    Passed first enzyme test

--- fwitter/js/splash.test.js
+++ fwitter/js/splash.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import SplashPage from './splash';
+import "isomorphic-fetch";
 
 describe('Testing the spash page', () => {
     it('can render a page without logging in', () => {
