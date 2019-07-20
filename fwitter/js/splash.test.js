533bf3230e62c6056bbe6792cd6dd7cb05cce268
Augustine Osagie <osagie98@gmail.com>
2019-07-20 13:59:31 -0500

    Reinstalled frontend

--- fwitter/js/splash.test.js
+++ fwitter/js/splash.test.js
@@ -8,3 +8,9 @@ describe('<SplashPage />', () => {
        shallow(<SplashPage />);
      });
  });
+
+ describe('A new Splash page test', () => {
+  it('can look for certain text', () => {
+     const wrapper = shallow(<SplashPage />);
+   });
+});
