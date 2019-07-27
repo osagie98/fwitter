91b6699ca589dfcf7664d6069a07c46e42b16ff8
Augustine Osagie <osagie98@gmail.com>
2019-07-27 14:41:27 -0500

    Modified tweet api to fit test

--- fwitter/api/tweet.py
+++ fwitter/api/tweet.py
@@ -11,18 +11,6 @@ import requests
 from fwitter.util.encrypt import hash_password
 import pdb
 
-@api_bp.route('/tweet', methods=['GET', 'POST'])
+@api_bp.route('/tweet', methods=['GET', 'POST', 'PATCH', 'DELETE'])
 def tweet():
     """Get a tweet from the database, or post a new one"""
-
-
-@api_bp.route('/like_tweet')
-def like_tweet():
-    """Post a like to a requested tweet"""
-
-    if flask.request.method == 'GET':
-        print('test')
-    elif flask.request.method == 'POST':
-        print('test')
-    else:
-        return {}, 403
