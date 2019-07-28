a58663294928e98056db410c146598d0014917f5
Augustine Osagie <osagie98@gmail.com>
2019-07-27 23:25:44 -0500

    Started on tweet api

--- fwitter/api/tweet.py
+++ fwitter/api/tweet.py
@@ -14,3 +14,17 @@ import pdb
 @api_bp.route('/tweet', methods=['GET', 'POST', 'PATCH', 'DELETE'])
 def tweet():
     """Get a tweet from the database, or post a new one"""
+    if 'username' not in flask.session:
+        return {}, 403
+
+    if flask.request.method == 'GET':
+        return {}, 404
+    elif flask.request.method == 'POST':
+        
+        return {}, 404
+    elif flask.request.method == 'PATCH':
+        return {}, 404
+    elif flask.request.method == 'DELETE':
+        return {}, 404
+    else:
+        return {}, 403
